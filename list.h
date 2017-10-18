36fb8fd4da5da1b03abed8ecb6cb77a31fe1eed4
Devin Cenatiempo <dcenatiempo@gmail.com>
2017-10-17 22:23:35 -0600

    Download Stub files from lab

---
+++ list.h
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * Header:
+ *    LIST
+ * Summary:
+ *    A linked list abstract data type
+ * Author
+ *    Devin Cenatiempo
+ ************************************************************************/
+
+#ifndef LIST_H
+#define LIST_H
+
+//custom namespace
+
+
+#endif // LIST_H
